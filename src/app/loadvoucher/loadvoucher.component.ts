dc4ac4d1428dac53ce6fe6dbf94e1d8815d1a17f
Luka <lukhanyo95@gmail.com>
2024-05-16 14:23:49 +0200

    update the loadvoucher component logic

--- src/app/loadvoucher/loadvoucher.component.ts
+++ src/app/loadvoucher/loadvoucher.component.ts
@@ -1,144 +1,146 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { VoucherloadedService } from 'src/services/voucherloaded.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-loadvoucher',
   templateUrl: './loadvoucher.component.html',
   styleUrls: ['./loadvoucher.component.scss'],
 })
 export class LoadvoucherComponent implements OnInit {
-  countdown: number=0;
+  countdown: number = 0;
   timer: any;
   redeemedVoucher!: string;
   expirationTime!: number;
-  voucherCode!: string 
+  voucherCode!: string;
 
   isTimerExpired: boolean = false;
 
   voucherForm: FormGroup = new FormGroup({
     voucher: new FormControl('', Validators.required),
   });
-  
-  constructor(private _voucherService: VoucherloadedService) {}
+
+  constructor(
+    private _voucherService: VoucherloadedService,
+    private _router: Router
+  ) {}
 
   message?: string;
-  message_class = ""
   isLoaded: boolean = false;
+  isHidden: boolean = false;
+
   oneDayVouchers = ['123456789124', '123456789125'];
-  threeDaysVouchers = ['123456789126', '123456789127', '123456789128'];
-  sevenDaysVouchers = ['123456789133', '123456789144', '123456789155'];
 
   days: number = 0;
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
-  
+
   ngOnInit(): void {
-
     //saves on the localStorage
-      const redeemedVoucher = localStorage.getItem('redeemedVoucher');
-      const expirationTime = localStorage.getItem('expirationTime');
+    const redeemedVoucher = localStorage.getItem('redeemedVoucher');
+    const expirationTime = localStorage.getItem('expirationTime');
 
-      if (redeemedVoucher && expirationTime) {
-          this.redeemedVoucher = redeemedVoucher;
-          this.expirationTime = Number(expirationTime);
-          this.startCountdown();
-      }
+    if (redeemedVoucher && expirationTime) {
+      this.redeemedVoucher = redeemedVoucher;
+      this.expirationTime = Number(expirationTime);
+      this.isLoaded = true;
+      this.isHidden = true;
+      this.startCountdown();
+    } else {
+      this.isLoaded = false;
+      this.isHidden = false;
+    }
   }
 
-    loadVoucher() {
-    const array = []
+  loadVoucher() {
+    let { voucher } = this.voucherForm.value;
 
-    const { voucher } = this.voucherForm.value;
-   
     this.message = this._voucherService.invalidVoucher(voucher);
 
     if (this.isLoaded === false) {
       if (this.oneDayVouchers.includes(voucher)) {
-        this.message = 'Succesfully loaded';
-        this.message_class = 'success'
+        setTimeout(() => {
+          this.message = '';
+        }, 2000);
+        this.message = 'Successfuly loaded!';
         this.isLoaded = true;
-        // this.isloasdedFrom = true
-        localStorage.setItem('isLoaded', this.isLoaded.toString())
-        
-        if (this.oneDayVouchers.includes(voucher)) {
-          this.redeemVoucher();
-        }  
+        localStorage.setItem('isLoaded', this.isLoaded.toString());
+        this.redeemVoucher();
+      } else {
+        setTimeout(() => {
+          this.message = '';
+        }, 2000);
+        this.message = 'Invalid Voucher!';
       }
     } else if (this.isLoaded === true) {
-      window.location.href = 'https://www.google.com/';
-      this.voucherForm;
-      array.push(this.isLoaded)
-      
-    }
-
-    if (this.getRemainingTime() > 0 && array.includes(voucher)) {
-      this.message = 'Seems like the voucher has already been loaded!';
+      window.open('https://www.google.com/', '_target');
     }
   }
 
-      startCountdown() {
-      this.countdown = this.getRemainingTime();
+  startCountdown() {
+    this.countdown = this.getRemainingTime();
 
-      this.timer = setInterval(() =>
-      {
+    this.timer = setInterval(() => {
       this.countdown--;
-        if (this.countdown <= 0) {
+      if (this.countdown <= 0) {
         this.stopCountdown();
-        }
-      }, 1000); // Update everysecond
       }
-      stopCountdown() {
-      clearInterval(this.timer);
-      // You may want to reset the voucher state here if needed
-      }
-
-      formatTime(totalSeconds: number):string {
-        const days = Math.floor(totalSeconds / (24 * 60 * 60));
-        const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-        
-        return `${this.pad(days)}:${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`;
-      }
-
-      private pad(value: number):string {
-        return value < 10 ? `0${value}`: `${value}`;
-      }
-
-      getRemainingTime(): number {
-        const currentTime = Math.floor(new Date().getTime() / 1000);
-        const remainingTime = this.expirationTime - currentTime;
-        
-        return remainingTime > 0 ? remainingTime : 0;
-      }
-
-        redeemVoucher() {
-          const { voucher } = this.voucherForm.value;
-          this.redeemedVoucher = voucher;
-          
-          if(!this.oneDayVouchers.includes(voucher)){
-            console.log("~ Invalid Voucher")
-            return
-          }
- 
-          this.expirationTime = this.calculateExpirationTime();
-
-          localStorage.setItem('redeemedVoucher', this.redeemedVoucher);
-          localStorage.setItem('expirationTime',
-            JSON.stringify(this.expirationTime));
-
-          this.startCountdown()
-        }
-
-        private calculateExpirationTime(): number {
-
-        const currentTimestamp = Math.floor(new Date().getTime() /1000);
-        return currentTimestamp -1 + 24 * 60; // 24 hours in seconds  
+    }, 1000);
   }
 
-  twitter(){
-    window.location.href="twitter.com"
+  stopCountdown() {
+    clearInterval(this.timer);
+    localStorage.removeItem('redeemedVoucher');
+    window.location.reload();
+  }
+
+  formatTime(totalSeconds: number): string {
+    const days = Math.floor(totalSeconds / (24 * 60 * 60));
+    const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    if (days >= 0 && hours >= 0 && minutes >= 0 && seconds >= 0) {
+      return `${this.pad(days)}:${this.pad(hours)}:${this.pad(
+        minutes
+      )}:${this.pad(seconds)}`;
+    }
+
+    return '00:00:00:00';
+  }
+
+  private pad(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+
+  getRemainingTime(): number {
+    const currentTime = Math.floor(new Date().getTime() / 1000);
+    if (currentTime != this.expirationTime) {
+      const remainingTime = this.expirationTime - currentTime;
+      return remainingTime > 0 ? remainingTime : 0;
+    }
+    return 0;
+  }
+
+  redeemVoucher() {
+    const { voucher } = this.voucherForm.value;
+    this.redeemedVoucher = voucher;
+
+    this.expirationTime = this.calculateExpirationTime();
+
+    localStorage.setItem('redeemedVoucher', this.redeemedVoucher);
+    localStorage.setItem('expirationTime', JSON.stringify(this.expirationTime));
+
+    this.startCountdown();
+  }
+
+  private calculateExpirationTime(): number {
+    const currentTimestamp = Math.floor(new Date().getTime() / 1000);
+    return currentTimestamp - 1 + 1 * 60;
+  }
+
+  twitter() {
+    window.location.href = 'twitter.com';
   }
 }
